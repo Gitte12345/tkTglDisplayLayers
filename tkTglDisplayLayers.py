2889881184e1e1c4211bf570abd5428c22675991
gitte12345 <tkutsch99@gmail.com>
2020-02-11 11:38:54 +0100

    better

--- tkTglDisplayLayers.py
+++ tkTglDisplayLayers.py
@@ -1,12 +1,22 @@
 # tkTglDisplayLayers.py
 
 import maya.cmds as cmds
+import maya.mel as mel
 
 def tkTglDisplayLayers():
     displayLayer = cmds.ls(type='displayLayer', l=1)
     if displayLayer:
-        state = cmds.getAttr(displayLayer[0] + '.visibility')
         for layer in displayLayer:
-            cmds.setAttr(layer + '.visibility', 1-state)
+            split = layer.split(':')
+            if len(split) == 1 and layer != 'defaultLayer':
+                state = cmds.getAttr(layer + '.visibility')
+
+    for layer in displayLayer:
+        split = layer.split(':')
+        if len(split) == 1 and layer != 'defaultLayer':
+            layerState = 1-(state*1)
+            cmd2 = 'setDisplayLayerVisibility("' + str(layer) + '", ' + str(layerState) + ')'
+            mel.eval(cmd2)
 
 tkTglDisplayLayers()
+
